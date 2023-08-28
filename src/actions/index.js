1a1fe41f901b4255d10114c3d103e97b6f2dda6b
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-28 16:53:42 +0300

    Action bakıldı

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 export const RESETMEMORY = "RESETMEMORY";
@@ -10,7 +9,21 @@ export const RESET = "RESET";
 export const addOne = () => {
   return { type: ADD_ONE };
 };
-
+export const reset = () => {
+  return { type: RESET };
+};
+export const memory = () => {
+  return { type: MEMORY };
+};
+export const resetMemory = () => {
+  return { type: RESETMEMORY };
+};
+export const addMemory = () => {
+  return { type: ADDMEMORY };
+};
 export const applyNumber = (number) => {
   return { type: APPLY_NUMBER, payload: number };
+};
+export const changeOperation = (operation) => {
+  return { type: CHANGE_OPERATION, payload: operation };
 };
