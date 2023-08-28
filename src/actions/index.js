7fa3910bbfd1cbe6396f0fad3956b29418c00b6b
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-28 16:43:29 +0300

    Reducer içine bakıldı

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,15 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const RESETMEMORY = "RESETMEMORY";
+export const MEMORY = "MEMORY";
+export const ADDMEMORY = "ADDMEMORY";
+export const RESET = "RESET";
 
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
